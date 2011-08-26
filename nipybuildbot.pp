01cc208d48c165189237ab52f6356d7057c120f1
Paul Ivanov <pivanov314@gmail.com>
2011-08-25 22:52:51 -0700

    ran it on apxumed just now

--- nipybuildbot.pp
+++ nipybuildbot.pp
@@ -5,23 +5,35 @@
 # and after 
 # file {'/home/buildbot/nipy-slave/info/admin':
 
+#group {'buildout':
+#    ensure => present,
+#}
+
 user { 'buildbot':
     ensure     => present,
+#groups => 'buildout',
         shell => '/bin/bash',
         home       => '/home/buildbot',
         managehome => true,
+#require => Group['buildout']
 }
 
-package { "buildbot": ensure => installed } # get buildbot from the system package manager
 
 File{
     ensure => 'present',
         owner => 'buildbot',
-        group => 'buildbot',
+        require => User['buildbot']
 }
 
+package { "buildbot": ensure => installed } # get buildbot from the system package manager
+
+file { '/home/buildbot':
+     ensure => directory,
+     require => User['buildbot']
+ }
 file { '/home/buildbot/.buildout':
      ensure => directory,
+     require => File['/home/buildbot']
  }
 
 
@@ -37,20 +49,21 @@ Exec {
     logoutput => true,
 }
 
-exec { 'create .local':
-command => "local processor=$(uname -p)
-local PYVER=$(python -ESV 2>&1);
-local PYVER_MAJOR=$(echo ${PYVER:7:3})
-local pypath=python${PYVER_MAJOR}/site-packages
-if [[ $processor == 'x86_64' ]]; then
-    mkdir $HOME/.local/lib64/$pypath
-fi
-mkdir $HOME/.local/lib/$pypath
-echo $HOME/.local/lib/$pypath
-",
-creates => '/home/buildout/.local/lib',
-user=>'buildbot',
-}
+#exec { 'create .local':
+#command => "local processor=$(uname -p)
+#local PYVER=$(python -ESV 2>&1);
+#local PYVER_MAJOR=$(echo ${PYVER:7:3})
+#local pypath=python${PYVER_MAJOR}/site-packages
+#if [[ $processor == 'x86_64' ]]; then
+#    mkdir $HOME/.local/lib64/$pypath
+#fi
+#mkdir $HOME/.local/lib/$pypath
+#echo $HOME/.local/lib/$pypath
+#",
+#creates => '/home/buildout/.local/lib',
+#user=>'buildbot',
+#require => User['buildbot']
+#}
 
 file { '/home/buildbot/.buildout/default.cfg':
     ensure => present,
@@ -77,7 +90,7 @@ file { '/home/buildbot/.buildout/configs':
 #}
 
 exec {'nipy-slave':
-command=>'buildbot create-slave nipy-slave nipy.bic.berkeley.edu:9989 pi-ykcyc bongo-builder',
+command=>'buildbot create-slave nipy-slave nipy.bic.berkeley.edu:9989 pi-ApxuMed bongo-builder',
 creates => '/home/buildbot/nipy-slave',
 require => Package['buildbot']
 }
@@ -90,29 +103,30 @@ cat /proc/cpuinfo | grep -i 'model name' >> ~buildbot/nipy-slave/info/host
 cat /proc/meminfo | grep -i MemTotal >> ~buildbot/nipy-slave/info/host
 ",
 mode => 755,
-require => Exec['nipy-slave']
+require => [Exec['nipy-slave'], User['buildbot']]
 }
 
 file {'/home/buildbot/nipy-slave/info/admin':
         content=> "Paul Ivanov <pi.berkeley.edu>",
-require => Exec['nipy-slave']
+require => [Exec['nipy-slave'], User['buildbot']]
 }
 
 exec {'set_info':
 command => '/home/buildbot/set_info',
 #subscribe => File['/home/buildbot/set_info'],
-require => File['/home/buildbot/set_info']
+require => [File['/home/buildbot/set_info'], Exec['nipy-slave'], User['buildbot']]
 }
 
 cron {'nipy buildbot':
     command => 'buildbot start /home/buildbot/nipy-slave',
     user => 'buildbot',
-    special => 'reboot'
+    special => 'reboot',
+    require => User['buildbot']
 }
 
 exec {'start buildslave':
     command => 'buildbot restart /home/buildbot/nipy-slave ;', 
     user => 'buildbot',
     cwd => '/home/buildbot',
-    require => [Exec['nipy-slave'],Exec['set_info'], File['/home/buildbot/nipy-slave/info/admin']],
+    require => [Exec['nipy-slave'],Exec['set_info'], File['/home/buildbot/nipy-slave/info/admin'],User['buildbot']],
 }
