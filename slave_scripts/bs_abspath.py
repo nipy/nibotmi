311dccce73f14d4a6b966b4dceb5dc7b55c4c9e0
Matthew Brett <matthew.brett@gmail.com>
2013-04-18 16:03:05 -0700

    RF: move bs scripts into single script file

--- slave_scripts/bs_abspath.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-print(os.path.abspath(sys.argv[1]))
---
+++ slave_scripts/bs_tools.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+""" Utilities for the buildslave """
+import os
+import sys
+from glob import glob
+from optparse import OptionParser
+
+
+def path_prepend_str(search_dir):
+    search_dir = os.path.abspath(search_dir)
+    if os.path.sep == '/':
+        print(search_dir + "/bin" + os.path.pathsep)
+    elif os.path.sep == '\\':
+        print(search_dir + "\\Scripts\\" + os.path.pathsep)
+    else:
+        raise RuntimeError("Oh dear, funny path sep")
+
+
+def main():
+    usage = """%prog [options] <command> <parameters>
+where <command> can be:
+
+path-prepend-dir
+abspath
+pjoin
+glob
+"""
+    parser = OptionParser(usage)
+    opts, args = parser.parse_args()
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(-1)
+    command = args.pop(0)
+    if command == 'path-prepend-str':
+        path_prepend_str(*args)
+    elif command == 'abspath':
+        print(os.path.abspath(os.path.join(*args)))
+    elif command == 'pjoin':
+        print(os.path.join(*args))
+    elif command == 'glob':
+        globber = os.path.join(*args)
+        print('\n'.join(sorted(glob(globber))))
+    else:
+        parser.print_help()
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    main()
